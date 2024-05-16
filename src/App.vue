025d4d12fb1c9575c232c7e2e13f6409a57a039b
drons2 <d.ronski@icloud.com>
2024-05-16 16:54:44 +0200

    Changed NavButtons

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,10 @@
       <img src="@/assets/images/Q04_HTW_Berlin_Logo_quer_pos_FARBIG_RGB.jpg" alt="HTW Berlin Logo" width="300" height="70">
     </div>
     <nav>
-      <button class=" shadow-sm btn btn-outline-primary" style="margin-right: 8px;"><RouterLink to="/">Meine Kurse</RouterLink></button>
-      <button class="shadow-sm btn btn-outline-primary" style="margin-right: 8px;"><RouterLink to="/allekurse">Alle Kurse</RouterLink></button>
-      <button class="shadow-sm btn btn-outline-primary"><RouterLink to="/kalender">Kalender</RouterLink></button>
+      <RouterLink to="/"><button class=" shadow-sm btn btn-outline-primary" style="margin-right: 8px;">Meine Kurse</button></RouterLink>
+      <RouterLink to="/allekurse"><button class="shadow-sm btn btn-outline-primary" style="margin-right: 8px;">Alle Kurse</button></RouterLink>
+      <RouterLink to="/kalender"><button class="shadow-sm btn btn-outline-primary">Kalender</button></RouterLink>
+
     </nav>
   </header>
   <body>
