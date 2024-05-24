cb61dcee7929b1ee56ba599d9f90c9599d611ae2
lawszki <d.ronski@icloud.com>
2024-05-24 15:06:41 +0200

    Frontend to Backend communication added.

---
+++ src/components/LoadButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const kurse : string []= [];
+
+function loadKurse() {
+  const baseUrl = process.env.VUE_APP_BACKEND_BASE_URL;
+  const endpoint = baseUrl + "/entries";
+  const requestOptions: any = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  fetch(endpoint, requestOptions)
+    .then(res => res.json())
+    .then(result => {
+      result.forEach((entry:any) => kurse.push(entry));
+    })
+    .catch(error => console.log('error', error));
+}
+</script>
+
+<template>
+  <button @click="loadKurse">Load Kurse</button>
+</template>
+
+<style scoped>
+/* Dein CSS */
+</style>
