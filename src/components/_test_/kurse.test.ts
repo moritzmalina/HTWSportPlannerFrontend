2f7ae9ee05854794b0141eda4ad13603413be80a
moritzmalina <80785467+moritzmalina@users.noreply.github.com>
2024-06-28 16:28:47 +0200

    frontend tests

---
+++ src/components/_test_/kurse.test.ts
@@ -0,0 +1,86 @@
+import { describe, it, expect, vi } from 'vitest'
+import { shallowMount, flushPromises } from '@vue/test-utils'
+import MeineKurse from "@/components/MeineKurse.vue";
+import {kurse} from "@/scraper/testdata";
+import axios from 'axios'
+
+describe('MeineKurse', () => {
+    const emptyResponse: any[] = []
+    const twoItemResponse: any[] = [
+        {
+            id: '1',
+            courseName: 'Yoga',
+            weekDay: 'Mo',
+            courseTime: '10:00',
+            place: 'Test 1',
+            startDate: '2024-01-01',
+            endDate: '2024-06-01',
+            management: 'Lord Voldemort',
+            selected: true,
+            color: true,
+            description: 'Test 1'
+        },
+        {
+            id: '2',
+            courseName: 'Pilates',
+            weekDay: 'Di',
+            courseTime: '11:00',
+            place: 'Test 2',
+            startDate: '2024-01-02',
+            endDate: '2024-06-02',
+            management: 'Harry Potter',
+            selected: true,
+            color: true,
+            description: 'Test 2'
+        }
+    ]
+
+    vi.mock('axios')
+    vi.mock('@okta/okta-vue')
+
+
+
+
+    it('should render the title "Meine Kurse"', () => {
+        vi.mocked(axios, true).get.mockResolvedValueOnce({ data: emptyResponse })
+
+        const wrapper = shallowMount(MeineKurse)
+        expect(wrapper.text()).toMatch('Meine Kurse')
+    })
+
+    it('should render the courses from the backend', async () => {
+        vi.mocked(axios, true).get.mockResolvedValueOnce({ data: twoItemResponse })
+
+        const item = twoItemResponse[0].courseName
+        const wrapper = shallowMount(MeineKurse)
+
+        await flushPromises()
+
+        expect(wrapper.text()).toContain(item)
+    })
+
+    it('should render some courses when received from backend', async () => {
+        vi.mocked(axios, true).get.mockResolvedValueOnce({ data: twoItemResponse })
+
+        const wrapper = shallowMount(MeineKurse)
+
+        await flushPromises()
+
+        expect(wrapper.text()).toContain('Yoga')
+        expect(wrapper.text()).toContain('Pilates')
+    })
+
+
+    it('should render the header "Kurse hinzufügen"', () => {
+        vi.mocked(axios, true).get.mockResolvedValueOnce({ data: emptyResponse })
+
+        const wrapper = shallowMount(MeineKurse)
+        expect(wrapper.text()).toMatch('Kurs hinzufügen')
+    })
+
+    it('should render the label "Kursname"', () => {
+        vi.mocked(axios, true).get.mockResolvedValueOnce({ data: emptyResponse })
+        const wrapper = shallowMount(MeineKurse)
+        expect(wrapper.text()).toContain('Kursname')
+    })
+})
